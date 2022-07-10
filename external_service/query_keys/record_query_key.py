c68498694e014d0e43f9361e0304817381820b9d
Tung Pham <joseph.pham280996@gmail.com>
2022-07-09 23:45:04 -0400

    Update Requirements.txt + fix export ot file

--- external_service/query_keys/record_query_key.py
+++
@@ -1,5 +0,0 @@
-class RecordQueryKeys:
-    def __init__(self, data, type_id, session_id):
-        self.data = data
-        self.typeID = type_id
-        self.sessionID = session_id
--- src/dependencies/reading_ecg_sensor_data.py
+++ src/dependencies/reading_ecg_sensor_data.py
@@ -2,7 +2,19 @@ from datetime import datetime
 from random import random
 from time import sleep
 from src.models.recorded_datum import RecordedDatum
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
+# Create the I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the ADC object using the I2C bus
+ads = ADS.ADS1015(i2c)
+
+# Create single-ended input on channel 0
+chan = AnalogIn(ads, ADS.P1)
 
 def reading_ecg_sensor_data(data, status, store_idx):
     while status:
@@ -13,7 +25,7 @@ def reading_ecg_sensor_data(data, status, store_idx):
         data[store_idx].append(
             RecordedDatum(
                 time_stamp=datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
-                data=random()
+                data=chan.value
             )
         )
         sleep(0.01)
