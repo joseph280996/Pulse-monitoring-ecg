64e6f6cf261992983a9bbb31265f385fcd4cffed
Tung Pham <joseph.pham280996@gmail.com>
2023-06-05 23:57:31 -0400

    Add websocket controller

---
+++ src/application/controllers/WebSocket/sensor_controller.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, WebSocket
+
+router = APIRouter(
+    prefix="/record",
+    tags=["record"],
+    responses={404: {"description": "Not found"}}
+)
+
+@router.websocket("/")
+async def recording_handler(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
