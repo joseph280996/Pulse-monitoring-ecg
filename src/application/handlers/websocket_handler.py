888bc8e9906938eba7847f756009a38b806fa1c8
Tung Pham <joseph.pham280996@gmail.com>
2023-07-14 21:10:24 -0400

    Update WebSocketHandler to print out message first for testing

--- src/application/handlers/websocket_handler.py
+++ src/application/handlers/websocket_handler.py
@@ -1,14 +1,14 @@
-from starlette.endpoints import WebSocketEndpoint
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
-
-class WebSocketHandler(WebSocketEndpoint):
+class WebSocketHandler(WebSocket):
     encoding = 'bytes'
 
     async def on_connect(self, websocket):
         await websocket.accept()
 
     async def on_received(self, websocket, data):
+        print(f"Received message: {data}")
         await websocket.send_bytes()
 
-    async def on_disconnect(self, websocket, close_code):
-        pass
+    async def on_disconnect(self, _, close_code):
+        print(f"Websocket client disconnected, code={close_code}")
