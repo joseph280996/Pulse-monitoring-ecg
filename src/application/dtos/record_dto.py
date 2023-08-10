4ec251a45b7faedddf08f6f6848fb5c9cf6a0098
Tung Pham <joseph.pham280996@gmail.com>
2023-08-09 23:25:12 -0400

    Clean up any unused files

--- src/application/dtos/record_dto.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel
-
-class RecordDto(BaseModel):
-    operation_type_id: int
--- src/application/handlers/diagnosis_id_notify_handlers.py
+++ src/application/handlers/diagnosis_id_notify_handlers.py
@@ -1,9 +1,9 @@
 from src.application.dtos.diagnosis_notify_dto import DiagnosisNotifyDto
-from src.domain.factories.sensor_manager_factory import SensorManagerFactory
+from src.domain.managers.sensor_manager import EcgSensorManager
 
 
 def diagnosis_created_notify_handler(
     diagnosis_notify_dto: DiagnosisNotifyDto, service_factory=SensorManagerFactory()
 ):
-    service = service_factory.get_service().get_instance()
+    service = EcgSensorManager.get_instance()
     service.diagnosis_id = diagnosis_notify_dto.diagnosisId
