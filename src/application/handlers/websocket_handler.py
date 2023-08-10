6fd01bb386b3353f4cef6660720b515cd9cfd913
Tung Pham <joseph.pham280996@gmail.com>
2023-08-09 23:16:07 -0400

    Update the handler

--- src/application/handlers/websocket_handler.py
+++ src/application/handlers/websocket_handler.py
@@ -1,40 +1,50 @@
-from apscheduler.schedulers.base import STATE_STOPPED
-from fastapi import Depends, WebSocket
-from sqlalchemy.orm import Session
+from fastapi import WebSocket
 from src.domain.managers.sensor_manager import EcgSensorManager
-from src.infrastructure.services.database import get_db
 
 
 class WebSocketHandler(WebSocket):
-    __sensor_service = None
+    """The WebSocket connection and message handler
 
-    def get_sensor_service(self, db: Session = Depends(get_db)):
-        service_factory = EcgSensorManager()
-        if not self.__sensor_service:
-            self.__sensor_service = service_factory.get_service().get_instance(db)
-        return self.__sensor_service
+    This class handles any incoming connection, disconnection, and when message came in.
+    """
+    __sensor_manager = EcgSensorManager.get_instance()
 
     async def on_connect(self, websocket):
         await websocket.accept()
 
     async def send_sensor_message(self, websocket: WebSocket):
-        service = self.get_sensor_service()
-        data = service.get_data()
+        """Handle sending message back to the front end for visualization.
+
+        Get the current list of sensor data from data storage in manager,
+        JSON stringified it and send through Websocket to the frontend.
+        """
+        data = self.__sensor_manager.get_data()
         await websocket.send_json(data)
 
     async def on_received(self, websocket, data):
+        """Handle incoming WebSocket message
+
+        This function will received the WebSocket message and decides on action to take
+
+        Allowed message and associated actions:
+            "start": will start the sensor reading loop and data transmission loop to the UI
+            "stop": will pause the sensor reading loop and data transmission loop
+            "pause": same as stop
+        """
         print(f"Received message: {data}")
-        service = self.get_sensor_service()
         if data == "start":
-            service.start_reading_values()
+            self.__sensor_manager.start_reading_values()
         if data == "stop":
-            service.stop_reading_values()
+            self.__sensor_manager.stop_reading_values()
 
     async def on_disconnect(self, _, close_code):
+        """Handle a disconnection to the server
+
+        This function will pause the loop when the UI disconnect from the server.
+        """
         print(f"Websocket client disconnected, code={close_code}")
-        sensor_get_data_scheduler = self.__sensor_service.get_instance().scheduler
+        sensor_get_data_scheduler = self.__sensor_manager.scheduler
         if (
             sensor_get_data_scheduler.running
-            and not sensor_get_data_scheduler.state == STATE_STOPPED
         ):
             sensor_get_data_scheduler.pause()
